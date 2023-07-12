36299a7adc29651d1c1a27ac04fef253c9dbc29f
unknown <lndubourg@gmail.com>
2023-07-12 16:26:06 +0200

    fin refactor maintenabilité

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {NewCampingCarComponent} from "./camping-cars/components/new-camping-car/new-camping-car.component";
-import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {LandingPageComponent} from "./landing-page/components/landing-page/landing-page.component";
 import {DetailCcComponent} from "./camping-cars/components/detail-cc/detail-cc.component";
 import {ListeCcComponent} from "./camping-cars/components/liste-cc/liste-cc.component";
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from "@angular/common/http";
 import {CoreModule} from "./core/core.module";
 import {CampingCarsModule} from "./camping-cars/camping-cars.module";
+import {LandingPageModule} from "./landing-page/landing-page.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
     CoreModule,
-    CampingCarsModule
+    CampingCarsModule,
+    LandingPageModule
   ],
   bootstrap: [AppComponent]
 })
